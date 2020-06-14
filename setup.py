110a9b2996c6470ad51796f74b0e99ddb0b1dd06
Soheab <soheabdebeste@hotmail.com>
2020-06-14 20:47:26 +0200

    last i hope

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     version = '1.0',
     packages = ['alexflipnote_api'],
     url = 'https://github.com/Soheab/Alexflipnote_api',
+    download_url = 'https://github.com/Soheab/alexflipnote_api/archive/1.0.tar.gz',
     license = 'MIT',
     author = 'Soheab',
     author_email = '',
-    install_requires=['url_regex'],
+    install_requires=['aiohttp', 'url_regex'],
     description = 'A easy to use Wrapper for the AlexFlipnote API'
 )
