bb19204593b62829e66a06af7de024624c8edbf6
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-12-17 18:09:22 +0100

    Bump version for development

--- alexflipnote/__init__.py
+++ alexflipnote/__init__.py
@@ -5,7 +5,7 @@ from .models import *
 from .errors import *
 from .enums import *
 
-__version__ = "4.0.2"
+__version__ = "4.1.0a"
 __author__ = "Soheab"
 __license__ = "MPL-2.0"
 __description__ = "An easy to use Python Wrapper for the AlexFlipnote API"
