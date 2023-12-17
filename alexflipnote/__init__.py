562e7d7351c2e8ad51297ed516b5b1fccbb18f04
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-12-17 18:08:59 +0100

    Replace setup.py with pyproject.toml

--- alexflipnote/__init__.py
+++ alexflipnote/__init__.py
@@ -1,9 +1,11 @@
+"""An easy to use Python Wrapper for the AlexFlipnote API"""
+
 from .client import *
 from .models import *
 from .errors import *
 from .enums import *
 
-__version__ = "4.3.0a"
+__version__ = "4.0.2"
 __author__ = "Soheab"
 __license__ = "MPL-2.0"
 __description__ = "An easy to use Python Wrapper for the AlexFlipnote API"
